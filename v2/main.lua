202d8eb0b9042781bb83829414777e094a0684c4
sandrorsjunior <sandrorsjunior@outlook.com>
2026-03-05 10:37:35 +0000

    create test pick and drop

--- v2/main.lua
+++ v2/main.lua
@@ -10,41 +10,65 @@ require("setting")
 
 -- get the first box position in order to perform pick up
 local box_angle_pick = 0
-local box_angle_drop = (math.pi)/2
+local box_angle_drop = 0
+
 local pick_position = get_position("pick", box_angle_pick)
-
 -- get the position of approach to pick
-local approach_point = get_approach(pick_position, box_angle_pick, {factorX = 1.1, factorY=1, factorZ=1.1})
-
+local approach_pick_point = get_approach(pick_position, box_angle_pick, {factorX = 1.1, factorY=1, factorZ=1.1})
 -- get the position over correct position but add few height
-local approach_soft = get_approach_soft(pick_position, 2)
+local approach_pick_soft =  get_approach_soft(pick_position, 2)
+
+
+local drop_position = get_position("drop", box_angle_drop)
+-- get the position of approach to pick
+local approach_drop_point = get_approach(drop_position, box_angle_drop, {factorX = 1.1, factorY=1, factorZ=1.1})
+-- get the position over correct position but add few height
+local approach_drop_soft =  get_approach_soft(pick_position, 2)
 
 
 print("Ange:")
 print(math.deg(box_angle_pick))
 print("pick_position:")
 print(inspect(pick_position))
-print("approach_point:")
-print(inspect(approach_point))
-print("approach_soft:")
-print(inspect(approach_soft))
+print("approach_pick_point:")
+print(inspect(approach_pick_point))
+print("approach_pick_soft:")
+print(inspect(approach_pick_soft))
+
+------> PICK
 
 MovJ({pose=home_pos["pose"]}, {user=0, tool=4, a = 10, v = 10})
 
-MovJ({pose=approach_point["pose"]}, {user=7, tool=4, a = 10, v = 10})
+MovJ({pose=approach_pick_point["pose"]}, {user=7, tool=4, a = 10, v = 10})
 
-MovJ({pose=approach_soft["pose"]}, {user = 7, tool=4, a = 10, v = 10})
+MovJ({pose=approach_pick_soft["pose"]}, {user = 7, tool=4, a = 10, v = 10})
 
 MovL({pose=pick_position["pose"]}, {user = 7, tool = 4, a = 10, v = 10 })
 
 DO(14, ON)
 
-MovL({pose=approach_soft["pose"]}, {user = 7, tool=4, a = 10, v = 10})
+MovL({pose=approach_pick_soft["pose"]}, {user = 7, tool=4, a = 10, v = 10})
 
 Wait(500)
 
-MovJ({pose=approach_point["pose"]}, {user=7, tool=4, a = 10, v = 10})
+MovJ({pose=approach_pick_point["pose"]}, {user=7, tool=4, a = 10, v = 10})
 
 MovJ({pose=home_pos["pose"]}, {user=0, tool=4, a = 10, v = 10})
 
+------> DROP
 
+MovJ({pose=approach_drop_point["pose"]}, {user=6, tool=4, a = 10, v = 10})
+
+MovJ({pose=approach_drop_soft["pose"]}, {user = 6, tool=4, a = 10, v = 10})
+
+MovL({pose=drop_position["pose"]}, {user = 6, tool = 4, a = 10, v = 10 })
+
+DO(14, OFF)
+
+Wait(500)
+
+MovL({pose=approach_drop_soft["pose"]}, {user = 6, tool=4, a = 10, v = 10})
+
+MovJ({pose=approach_drop_point["pose"]}, {user=6, tool=4, a = 10, v = 10})
+
+MovJ({pose=home_pos["pose"]}, {user=0, tool=4, a = 10, v = 10})
