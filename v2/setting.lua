f0d8c59748aacc04453a2be393814a86b4a0d1ca
sandrorsjunior <sandrorsjunior@outlook.com>
2026-03-05 08:33:31 +0000

    home_pose created

--- v2/setting.lua
+++ v2/setting.lua
@@ -4,7 +4,7 @@ box_width = 250
 box_height = 210
 
 -- 2. Configuração do Palete
-pallet_rows = 2-- Caixas no eixo Y
+pallet_rows = 2 -- Caixas no eixo Y
 pallet_cols = 2 -- Caixas no eixo X
 max_layers = 1
 
@@ -12,24 +12,36 @@ PALLET_USER_6 = 6
 PALLET_USER_7 = 7
 
 
+home_pos = {
+    user = 0,
+    tool = 4,
+    pose = {
+        -115.0697,
+        639.2179,
+        -36.3228,
+        0,
+        0,
+        -44.528
+    }
+}
 
 -- 4. offsets
 offset_vacuum = 5 -- Define quanto o gripper vai deformar sua espuma
 -- 4.1 offset pl
 offset_pl = {
-  x= 158.4987,
-  y= 126.3081,
-  theta= 135.4723
+    x = 158.4987,
+    y = 126.3081,
+    theta = 135.4723
 }
 
 -- 4.2 offset pr
 offset_pr = {
-  x= -144.9174,
-  y= 121.7384,
-  theta= 135.9192
+    x = -144.9174,
+    y = 121.7384,
+    theta = 135.9192
 }
 
--- 0. Controler 
+-- 0. Controler
 current_layer = max_layers
 current_row = 1
 current_col = 1
