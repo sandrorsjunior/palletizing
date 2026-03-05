ac9f763224654eefd5404a6e7d6d6ecf9d5f31e3
sandrorsjunior <sandrorsjunior@outlook.com>
2026-03-05 08:33:13 +0000

    get_approach test

--- v2/main.lua
+++ v2/main.lua
@@ -6,10 +6,21 @@ require("controller")
 require("setting")
 
 
-local pick_position = get_position("pick", (math.pi)/2)
+--goToHome()
 
+-- get the first box position in order to perform pick up
+local box_angle_pick = 0
+local box_angle_drop = (math.pi)/2
+local pick_position = get_position("pick", box_angle_pick)
+
+-- get the position of approach to pick
+local approach_point = get_approach("pink", pick_position, box_angle_pick, 1.2)
+
+
+print("Ange:")
+print(math.deg(box_angle_pick))
+print("pick_position:")
 print(inspect(pick_position))
-
-
-
+print("approach_point")
+print(inspect(approach_point))
 
